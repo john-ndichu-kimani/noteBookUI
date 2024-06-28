548edd7d03f4f929aef76e2132e91baba9e1a079
john ndichu <johnndichu104@gmail.com>
2024-06-28 18:52:53 +0300

    Add end to end test for simulating availability of each note item after being creating

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,24 +1,43 @@
 describe('Home component', () => {
-
   beforeEach(() => {
-    // Visit the home page before each test
     cy.visit('/');
   });
 
   it('should visit home page to view all the notes', () => {
-    // Check if the "Add a new note" button is visible
     cy.get('.newNote')
       .should('be.visible')
       .and('contain', 'Add a new note');
   });
 
   it('navigates to add new note form page', () => {
-    // Click the "Add a new note" button
+    cy.get('.newNote').click();
+    cy.url().should('include', '/add-note');
+    cy.get('form').should('be.visible');
+  });
+
+  it('should create a new note and display it on the home page', () => {
     cy.get('.newNote').click();
 
-    // Verify if the navigation to the form page happened (assuming URL changes or form appears)
-    cy.url().should('include', '/create'); // Update this if the URL structure is different
-    cy.get('form').should('be.visible'); // Assuming the form has a <form> tag
+
+    cy.get('input[name="title"]').type('Test Note');
+    cy.get('textarea[name="content"]').type('This is a test note.');
+
+
+    cy.get('button[type="submit"]').click();
+
+    cy.visit('/');
+    cy.contains('Test Note').should('be.visible');
+    cy.contains('This is a test note.').should('be.visible');
+  });
+
+  it('should delete a note when delete button is clicked', () => {
+
+    cy.get('.note-card').should('exist');
+
+
+    cy.get('.note-card').first().find('.delete-btn').click();
+
+    cy.get('.note-card').should('not.exist');
   });
 
 });
